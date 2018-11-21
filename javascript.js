e563afdcbf266375f8f0df3208b756f3e1112ab9
hwaidi <42866987+hwaidi@users.noreply.github.com>
2018-11-21 17:03:16 +0100

    First commit

---
+++ javascript.js
@@ -0,0 +1,38 @@
+alert("hello world");
+
+var a = [50 + 10] * 20;
+alert(a);
+
+
+var a = 20;
+var b = 20;
+var c = (a + b);
+
+if (a === b) {
+  alert("they are equal");
+} else {
+  alert("they are not equal");
+}
+
+var aa = 10;
+var bb = 20;
+var cc = 30;
+var dd = 40;
+
+if (aa === bb && cc < dd){
+  document.write("the condition is true")
+} else {
+  document.write("the condition is false")
+}
+
+
+var aaa = 10;
+var bbb = 20;
+var ccc = 30;
+var ddd = 40;
+
+if (aaa === bbb || ccc < ddd){
+  document.write("it is true")
+} else {
+  document.write("it is false")
+}
