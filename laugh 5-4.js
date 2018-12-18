8a5e17fd5657ed61ddcb212ad40380ebd574cf06
hwaidi <42866987+hwaidi@users.noreply.github.com>
2018-12-18 08:41:44 +0100

    laugh 5-4

---
+++ laugh 5-4.js
@@ -0,0 +1,11 @@
+
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+var laugh = function(howManyHa) {
+    return "ha".repeat(howManyHa) + "!";
+};
+
+console.log(laugh(3));  // hahaha!
+console.log(laugh(10)); // hahahahahahahahahaha!
